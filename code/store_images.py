24e7c662605fab6a42eace14537d8db4112cb7e5
chronosss1 <50720898+juanmiguelrl@users.noreply.github.com>
2022-06-12 21:26:48 +0200

    Update store_images.py

--- code/store_images.py
+++ code/store_images.py
@@ -9,5 +9,7 @@ def store_images_together(destination_path,input_dir):
     if not os.path.exists(destination_path):
         os.makedirs(destination_path)
 
-    for img in glob.glob(input_dir):
-        shutil.copy(img, destination_path+"\\"+get_filename(img))
+    for root, dirs, files in os.walk(input_dir):
+        for img in files:
+            path_file = os.path.join(root, img)
+            shutil.copy(path_file , destination_path)
